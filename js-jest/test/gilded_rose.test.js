3dc641a8956536afc98c66aa5b892cda709641a2
Elisabeth Post <elisabeth.post@centauricorp.com>
2023-10-04 13:14:38 -0600

    add intro pin down tests, update test command, update git ignore

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,32 @@
-const {Shop, Item} = require("../src/gilded_rose");
+const { Item, Shop } = require("../src/gilded_rose.js");
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+describe("Gilded Rose Pin Down Tests", () => {
+  test("Normal items should degrade in quality by 1 each day", () => {
+    let normalItem = new Item("normal", 10, 20); //build
+    const gildedRose = new Shop([normalItem]);
+
+    const items = gildedRose.updateQuality(); //operate
+
+    expect(items[0].quality).toBe(19); //check
+  });
+
+  test('Quality of "Aged Brie" should increase by 1 each day', () => {
+    let agedBrie = new Item("Aged Brie", 10, 20);
+    const gildedRose = new Shop([agedBrie]);
+
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+
+    expect(items[0].quality).toBe(21);
+  });
+
+  test('Quality of "Backstage passes" should increase by 3 when there are 5 days or less', () => {
+    let backstagePass = new Item(
+      "Backstage passes to a TAFKAL80ETC concert",
+      5,
+      20
+    );
+    const gildedRose = new Shop([backstagePass]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(23);
   });
 });
